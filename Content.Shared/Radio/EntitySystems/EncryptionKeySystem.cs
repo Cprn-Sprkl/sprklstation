35acf3ab8039abb1f8e6df71a36017348c344e13
Cprn-Sprkl <sprkl7992@gmail.com>
2025-09-16 04:33:58 -0400

    Improved Examines
    Fixes the syndicate key eating all keys behind it on the radio list. Returns the syndicate information to the key's examine, but not if it is in a headset.

--- Content.Shared/Radio/EntitySystems/EncryptionKeySystem.cs
+++ Content.Shared/Radio/EntitySystems/EncryptionKeySystem.cs
@@ -242,11 +242,10 @@ public sealed partial class EncryptionKeySystem : EntitySystem
             proto = _protoManager.Index<RadioChannelPrototype>(id);
 
             //Floof - Hide Syndicate key from examine
-            if (proto.ID == "Syndicate")
+            if (proto.ID == "Syndicate" & !HasComp<EncryptionKeyComponent>(examineEvent.Examined))
             {
-                return;
+                continue;
             }
-            ;
             //Floof - End
 
             var key = id == SharedChatSystem.CommonChannel
@@ -264,6 +263,12 @@ public sealed partial class EncryptionKeySystem : EntitySystem
         {
             if (HasComp<HeadsetComponent>(examineEvent.Examined))
             {
+                //Floof - Hide Syndicate key from examine
+                if (defaultChannel == "Syndicate")
+                {
+                    return;
+                }
+                //Floof - End
                 var msg = Loc.GetString("examine-headset-default-channel",
                 ("prefix", SharedChatSystem.DefaultChannelPrefix),
                 ("channel", defaultChannel),
